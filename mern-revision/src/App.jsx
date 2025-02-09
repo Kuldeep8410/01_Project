be9f7256581bda19512df03d1615ec258668a122
Lokesh1933 <167898603+Lokesh1933@users.noreply.github.com>
2025-02-09 16:23:52 +0530

    added one component

--- mern-revision/src/App.jsx
+++ mern-revision/src/App.jsx
@@ -1,26 +1,29 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
+
 import './App.css'
 // import Design from './signup-login/Design'
 import Signup from './signup-login/Signup'
 
 import AOS from 'aos'
 import Login from './signup-login/Login'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   useEffect (() =>{
        AOS.init({duration : 3000});
   },[])
 
   return (
+    <>
+    <Navbar />
    
       <div className='w-full h-screen bg-amber-400'>
         <Signup />
         <Login />
       </div>
+      </>
     
   )
 }
